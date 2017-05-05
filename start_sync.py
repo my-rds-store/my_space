97e7c586dbc072e19897385d4608f6743ff310d6
jiang_xmin <jiang_xmin@massclouds.com>
2017-05-05 17:51:53 +0800

    ....

--- start_sync.py
+++ start_sync.py
@@ -1,15 +1,15 @@
 #/usr/bin/env python
 
 import commands
-from time import sleep
+from  time import sleep
 
 #ret = commands.getoutput("git pull mc_origin master")
 
 while True:
     status,ret_str = commands.getstatusoutput("git pull mc_origin master")
     #print  status  
-    #print ret_str
-    if str(ret_str).find("Already up-to-date"):
+    print ret_str
+    if ret_str.find("Already up-to-date") != -1:
         print " Already up-to-date"
     else:
         print " clone ...building... "
