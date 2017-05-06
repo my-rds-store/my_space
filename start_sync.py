660a6204303b309675b3e2537ac66efcb9f91340
jxm_zn <jxm_zn@163.com>
2017-05-06 10:14:26 +0800

    start_sync.py

--- start_sync.py
+++ start_sync.py
@@ -4,24 +4,30 @@ import commands
 from  time import sleep
 import sys
 
-#ret = commands.getoutput("git pull mc_origin master")
+from loginit import init_logger
+import logging
+
+init_logger("/tmp/my_space.log")
+
 
 def build():
 
     ret = commands.getoutput("make html")
-    print ret
+    #print ret
+    logging.info(ret)
 
 while True:
     status,ret_str = commands.getstatusoutput("git pull mc_origin master")
     #print  status  
     #print ret_str
     if str(ret_str).find("Already up-to-date") != -1:
-        #print " Already up-to-date"
-        print " .",
-        sys.stdout.flush()
+        #print "Already up-to-date"
+        logging.info("Already up-to-date")
+        #print " .",
+        #sys.stdout.flush()
     else:
-        print "."
-        print "building... "
+        #print "."
+        #print "building... "
         build()
     sleep(2)
         
