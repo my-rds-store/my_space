b9763574781922acebc1e2e24c56e2131353c939
Jinagxumin <cjiangxumin@gmail.com>
2024-08-09 02:30:45 +0800

    updae ...

--- source/conf.py
+++ source/conf.py
@@ -31,26 +31,36 @@ import os
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['sphinx.ext.autodoc',
-     'sphinx.ext.doctest',
-     'sphinx.ext.intersphinx',
-     'sphinx.ext.todo',
-     'sphinx.ext.coverage',
-     'sphinx.ext.mathjax',  ###
-     'sphinx.ext.ifconfig',
-     'sphinx.ext.viewcode',
-     'sphinx.ext.githubpages',
-     'sphinx.ext.graphviz',   ### graphviz画图插
-     'recommonmark',           # sport markdown
-     'sphinx_markdown_tables', # sport markdown tables
-     'sphinx_math_dollar',     # sport math
-     'sphinx.ext.mathjax',     # sport math
-     'sphinxcontrib.inkscapeconverter' # svg convert to pdf
-     ]
+# extensions = ['sphinx.ext.autodoc',
+#      'sphinx.ext.doctest',
+#      'sphinx.ext.intersphinx',
+#      'sphinx.ext.todo',
+#      'sphinx.ext.coverage',
+#      'sphinx.ext.mathjax',  ###
+#      'sphinx.ext.ifconfig',
+#      'sphinx.ext.viewcode',
+#      'sphinx.ext.githubpages',
+#      'sphinx.ext.graphviz',   ### graphviz画图插
+#      'recommonmark',           # sport markdown
+#      'sphinx_markdown_tables', # sport markdown tables
+#      'sphinx_math_dollar',     # sport math
+#      'sphinx.ext.mathjax',     # sport math
+#      'sphinxcontrib.inkscapeconverter' # svg convert to pdf
+#      ]
 
-# source_parsers = {
-#     '.md': 'recommonmark.parser.CommonMarkParser',
-# }
+extensions = [
+    'recommonmark',
+    'sphinx_markdown_tables',
+    'sphinx_math_dollar',
+    'sphinx.ext.mathjax',
+    'sphinxcontrib.inkscapeconverter'
+    ]
+
+source_parsers = {
+    '.md': 'recommonmark.parser.CommonMarkParser',
+}
+source_suffix = ['.rst', '.md']
+
 
 # on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 # if on_rtd:
@@ -78,7 +88,7 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
+# source_suffix = ['.rst', '.md']
 #source_suffix = '.rst'
 
 # The master toctree document.
@@ -268,110 +278,15 @@ texinfo_documents = [
 # 
 
 # #######################################################################
-# # -- Options for PDF output --------------------------------------------------
+# # -- Options for graphviz  seting -------------------------------------
 # #######################################################################
-#   
-# # Grouping the document tree into PDF files. List of tuples
-# # (source start file, target name, title, author, options).
-# #
-# # If there is more than one author, separate them with \\.
-# # For example: r'Guido van Rossum\\Fred L. Drake, Jr., editor'
-# #
-# # The options element is a dictionary that lets you override
-# # this config per-document.
-# # For example,
-# # ('index', u'MyProject', u'My Project', u'Author Name',
-# # dict(pdf_compressed = True))
-# # would mean that specific document would be compressed
-# # regardless of the global pdf_compressed setting.
-# 
-# pdf_documents = [
-#   ('index', u'onpice', u'海贼王', u'hankcs'),
-# ]
-#   
-# # A comma-separated list of custom stylesheets. Example:
-# pdf_stylesheets = ['a4','zh_CN']
-#   
-# # Create a compressed PDF
-# # Use True/False or 1/0
-# # Example: compressed=True
-# #pdf_compressed = False
-#   
-# # A colon-separated list of folders to search for fonts. Example:
-# #pdf_font_path = ['C:\\Windows\\Fonts']
-#   
-# # Language to be used for hyphenation support
-# pdf_language = "zh_CN"
-#   
-# # Mode for literal blocks wider than the frame. Can be
-# # overflow, shrink or truncate
-# pdf_fit_mode = "shrink"
-#   
-# # Section level that forces a break page.
-# # For example: 1 means top-level sections start in a new page
-# # 0 means disabled
-# #pdf_break_level = 0
-#   
-# # When a section starts in a new page, force it to be 'even', 'odd',
-# # or just use 'any'
-# #pdf_breakside = 'any'
-#   
-# # Insert footnotes where they are defined instead of
-# # at the end.
-# #pdf_inline_footnotes = True
-#   
-# # verbosity level. 0 1 or 2
-# #pdf_verbosity = 0
-#   
-# # If false, no index is generated.
-# #pdf_use_index = True
-#   
-# # If false, no modindex is generated.
-# #pdf_use_modindex = True
-#   
-# # If false, no coverpage is generated.
-# #pdf_use_coverpage = True
-#   
-# # Documents to append as an appendix to all manuals.
-# #pdf_appendices = []
-#   
-# # Enable experimental feature to split table cells. Use it
-# # if you get "DelayedTable too big" errors
-# #pdf_splittables = False
-#   
-# # Set the default DPI for images
-# #pdf_default_dpi = 72
-#   
-# # Enable rst2pdf extension modules (default is only vectorpdf)
-# # you need vectorpdf if you want to use sphinx's graphviz support
-# #pdf_extensions = ['vectorpdf']
-#   
-# # Page template name for "regular" pages
-# #pdf_page_template = 'cutePage'
-#   
-# # Show Table Of Contents at the beginning?
-# # pdf_use_toc = False
-#   
-# # How many levels deep should the table of contents be?
-# pdf_toc_depth = 2
-#   
-# # Add section number to section references
-# pdf_use_numbered_links = False
-#   
-# # Background images fitting mode
-# pdf_fit_background_mode = 'scale'
 
-
-#######################################################################
-# -- Options for graphviz  seting -------------------------------------
-#######################################################################
-
-# 设置 graphviz_dot 路径
-graphviz_dot = 'dot'
-# 设置 graphviz_dot_args 的参数，这里默认了默认字体
-graphviz_dot_args = ['-Gfontname=Georgia', 
-                     '-Nfontname=Georgia',
-                     '-Efontname=Georgia']
-# 输出格式，默认png，这里我用svg矢量图
-graphviz_output_format = 'svg'
+# # 设置 graphviz_dot 路径
+# graphviz_dot = 'dot'
+# # 设置 graphviz_dot_args 的参数，这里默认了默认字体
+# graphviz_dot_args = ['-Gfontname=Georgia', 
+#                      '-Nfontname=Georgia',
+#                      '-Efontname=Georgia']
+# # 输出格式，默认png，这里我用svg矢量图
+# graphviz_output_format = 'svg'
 
