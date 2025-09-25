7a8305229a93dbfa5046a8b0a3d4a5fdebb90e58
promote <jiangxumin@sdpmjd.com>
2025-09-25 18:16:23 +0800

    updae ...

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 ##
-docker run -t --rm -v ~/Documents/my_space:/root/my_space --name my_space_build  my_space_build:v1 bash -c "cd my_space; make html -j5"
+# docker run -t --rm -v ~/Documents/my_space:/root/my_space --name my_space_build  my_space_build:v1 bash -c "cd my_space; make html -j5"
+ docker run -t --rm -v ~/Documents/my_space:/root/my_space --name my_space_build 192.168.2.100:8086/pm-autopilot/docs_build:v2 bash -c "cd my_space; make html -j5"
+#"192.168.2.100:8086/pm-autopilot/docs_build:v2"
